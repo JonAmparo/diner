3689caad17199e164ed4795eb58d2b13ee8e5175
Jonathan Amparo <jonathanmamparo@gmail.com>
2020-09-24 14:39:03 -0400

    Updates

---
+++ precache-manifest.d4a7daea42d8abff36f0f6888a23e2c1.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa232e6e8d0e290de8e4894fa6dd0c9",
+    "url": "/diner/index.html"
+  },
+  {
+    "revision": "2c84e1484f811f9e57c9",
+    "url": "/diner/static/js/2.87c91cc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/diner/static/js/2.87c91cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff170f9835d74343fad4",
+    "url": "/diner/static/js/main.0d1b9402.chunk.js"
+  },
+  {
+    "revision": "839e96ec43a854ec2809",
+    "url": "/diner/static/js/runtime-main.a111a15d.js"
+  },
+  {
+    "revision": "2af453916df814fcf426f993531834ca",
+    "url": "/diner/static/media/Spartan-Black.2af45391.ttf"
+  },
+  {
+    "revision": "a3b8dba87c8a969c604cab9f4267e628",
+    "url": "/diner/static/media/Spartan-Bold.a3b8dba8.ttf"
+  },
+  {
+    "revision": "94468be5b10313c7c3b7f7d0af5a8435",
+    "url": "/diner/static/media/Spartan-ExtraBold.94468be5.ttf"
+  },
+  {
+    "revision": "9d9d3e87960af91e1a7bc2fdf649825c",
+    "url": "/diner/static/media/Spartan-ExtraLight.9d9d3e87.ttf"
+  },
+  {
+    "revision": "9db064d86ea78b3a32b5c9d326739e15",
+    "url": "/diner/static/media/Spartan-Light.9db064d8.ttf"
+  },
+  {
+    "revision": "7bc456708901ba828fe5ee9803962ce5",
+    "url": "/diner/static/media/Spartan-Medium.7bc45670.ttf"
+  },
+  {
+    "revision": "940184baadcd8debdbd01951870c0ec9",
+    "url": "/diner/static/media/Spartan-Regular.940184ba.ttf"
+  },
+  {
+    "revision": "685afe2283f2d6006cfe243a161972d1",
+    "url": "/diner/static/media/Spartan-SemiBold.685afe22.ttf"
+  },
+  {
+    "revision": "f73c0eed7364f951dabf8b9059eabed4",
+    "url": "/diner/static/media/Spartan-Thin.f73c0eed.ttf"
+  },
+  {
+    "revision": "a424e9b5d4d1152d5b109131323e15c2",
+    "url": "/diner/static/media/beef-desktop-tablet@2x.a424e9b5.jpg"
+  },
+  {
+    "revision": "2fc61a366160db0724775da9b456768e",
+    "url": "/diner/static/media/beef-mobile@2x.2fc61a36.jpg"
+  },
+  {
+    "revision": "20a771578ef06b6d4f8dda8ca576baab",
+    "url": "/diner/static/media/chocolate-desktop-tablet@2x.20a77157.jpg"
+  },
+  {
+    "revision": "71d394d0f8f7544d9379bd685319e573",
+    "url": "/diner/static/media/chocolate-mobile@2x.71d394d0.jpg"
+  },
+  {
+    "revision": "2dfa948e2d2120d2691f8c77f2f12d05",
+    "url": "/diner/static/media/enjoyable-place-desktop@2x.2dfa948e.jpg"
+  },
+  {
+    "revision": "38e2cfbc2b1612884942257a41aaa943",
+    "url": "/diner/static/media/enjoyable-place-mobile@2x.38e2cfbc.jpg"
+  },
+  {
+    "revision": "36532372987f39ab653e3acd0f1d974a",
+    "url": "/diner/static/media/enjoyable-place-tablet@2x.36532372.jpg"
+  },
+  {
+    "revision": "dff4dca8b078e146049b0e015d3788d5",
+    "url": "/diner/static/media/family-gathering-desktop@2x.dff4dca8.jpg"
+  },
+  {
+    "revision": "da6024673bea269f0a01f13c2987a0f5",
+    "url": "/diner/static/media/family-gathering-mobile@2x.da602467.jpg"
+  },
+  {
+    "revision": "2ddf41787e398f36d2fd607744d9ad94",
+    "url": "/diner/static/media/family-gathering-tablet@2x.2ddf4178.jpg"
+  },
+  {
+    "revision": "c583b43fdb1ea1c8c7f61453645449c3",
+    "url": "/diner/static/media/hero-bg-desktop@2x.c583b43f.jpg"
+  },
+  {
+    "revision": "d69ea4f8353adc5effcc0ec0e2541aee",
+    "url": "/diner/static/media/hero-bg-desktop@2x.d69ea4f8.jpg"
+  },
+  {
+    "revision": "5a06d41ad690f8f040c4ddf6cab6beae",
+    "url": "/diner/static/media/hero-bg-mobile@2x.5a06d41a.jpg"
+  },
+  {
+    "revision": "f8e1664f5798769f36dc57ce000cef41",
+    "url": "/diner/static/media/hero-bg-mobile@2x.f8e1664f.jpg"
+  },
+  {
+    "revision": "a426bb488007c44d33eb46dc4963e0a4",
+    "url": "/diner/static/media/hero-bg-tablet@2x.a426bb48.jpg"
+  },
+  {
+    "revision": "b5bbd83a5b30addfcc89c3cf49d26c6e",
+    "url": "/diner/static/media/hero-bg-tablet@2x.b5bbd83a.jpg"
+  },
+  {
+    "revision": "9ee235724df584d44cd16efcb0d0a454",
+    "url": "/diner/static/media/locally-sourced-desktop@2x.9ee23572.jpg"
+  },
+  {
+    "revision": "8c0135c080eca620327740f425215391",
+    "url": "/diner/static/media/locally-sourced-mobile@2x.8c0135c0.jpg"
+  },
+  {
+    "revision": "6b39764dd020d7e41c9068371c8c5cf8",
+    "url": "/diner/static/media/locally-sourced-tablet@2x.6b39764d.jpg"
+  },
+  {
+    "revision": "932ec5bc8d11397b3eca4d28efa9221b",
+    "url": "/diner/static/media/logo.932ec5bc.svg"
+  },
+  {
+    "revision": "2f7c5e37374f90b9a1da606a086410ab",
+    "url": "/diner/static/media/pattern-curve-bottom-right.2f7c5e37.svg"
+  },
+  {
+    "revision": "2465b8690b7e5ee4df724892e32be286",
+    "url": "/diner/static/media/pattern-curve-top-left.2465b869.svg"
+  },
+  {
+    "revision": "8e92e19517a7b2e6058a1644248e5b8e",
+    "url": "/diner/static/media/pattern-curve-top-right.8e92e195.svg"
+  },
+  {
+    "revision": "77989ac9c8c298c7c7b164f724c3dcb9",
+    "url": "/diner/static/media/pattern-lines.77989ac9.svg"
+  },
+  {
+    "revision": "55d123bffefc7ceb476ea74d8eeaa328",
+    "url": "/diner/static/media/ready-bg-desktop@2x.55d123bf.jpg"
+  },
+  {
+    "revision": "0a4f3ad0f853ca4b86b02134e48a4f12",
+    "url": "/diner/static/media/ready-bg-mobile@2x.0a4f3ad0.jpg"
+  },
+  {
+    "revision": "0028f13453515543fb69c786612050b0",
+    "url": "/diner/static/media/ready-bg-tablet@2x.0028f134.jpg"
+  },
+  {
+    "revision": "831149fd1ba542fbe0886d29777ff837",
+    "url": "/diner/static/media/salmon-desktop-tablet@2x.831149fd.jpg"
+  },
+  {
+    "revision": "d31c4835512dc92ffbd2f004466fc01d",
+    "url": "/diner/static/media/salmon-mobile@2x.d31c4835.jpg"
+  },
+  {
+    "revision": "9c157134af789f2116b26430be267e40",
+    "url": "/diner/static/media/social-events-desktop@2x.9c157134.jpg"
+  },
+  {
+    "revision": "a0cd97c1cc4b68c244b690d2d040e15f",
+    "url": "/diner/static/media/social-events-mobile@2x.a0cd97c1.jpg"
+  },
+  {
+    "revision": "e15ebfcdf7ff4c869bb88fba6d4ffe0e",
+    "url": "/diner/static/media/social-events-tablet@2x.e15ebfcd.jpg"
+  },
+  {
+    "revision": "3a427a80553bf94f63a0bc98d1b40a42",
+    "url": "/diner/static/media/special-events-desktop@2x.3a427a80.jpg"
+  },
+  {
+    "revision": "08e5f1e0b6e55c9ebc5e2416be107839",
+    "url": "/diner/static/media/special-events-mobile@2x.08e5f1e0.jpg"
+  },
+  {
+    "revision": "e3251142f462b6e1b6e765d3fc8f59a0",
+    "url": "/diner/static/media/special-events-tablet@2x.e3251142.jpg"
+  }
+]);
